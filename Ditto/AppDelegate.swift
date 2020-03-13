a2afad72dd6d25b614695a1fbdc59d559b293788
dun silent <tichydusan@yahoo.com>
2020-03-14 00:19:42 +0100

    Initial Commit

---
+++ Ditto/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ditto
+//
+//  Created by Dusan Tichy on 14/03/2020.
+//  Copyright © 2020 dti. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ditto/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ditto
+//
+//  Created by Dusan Tichy on 14/03/2020.
+//  Copyright © 2020 dti. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
